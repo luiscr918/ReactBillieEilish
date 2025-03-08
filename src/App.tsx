7c1bd3c41666e8a7883901090d73bbce74673bad
luiscr918 <lc9107040@gmail.com>
2025-03-08 18:38:53 -0500

    Enrutamiento listo bases para comenzar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
 
 
 function App() {
 
 
   return (
-    <>
-      <h3>hola</h3>
-
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
