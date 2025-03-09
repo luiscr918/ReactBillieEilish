3db7815189206d1b05f7c5112da78ad02b1fd03e
luiscr918 <lc9107040@gmail.com>
2025-03-09 18:41:30 -0500

    navbar listo solo falta la lupa inicio de sesion y carrito que eso dejaré para el final

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const NavbarComponent = () => {
+    const [desplegableTienda, setdesplegableTienda] = useState(false);
+    const [menuAbierto, setMenuAbierto] = useState(false);
+
     return (
         <>
             {/* <!--Bar Nav--> */}
-            <nav className="sticky top-0 z-50 bg-white border-gray-200  dark:border-gray-700 component-background-billie ">
+            <nav className="sticky top-0 z-50 bg-white border-gray-200 dark:border-gray-700 component-background-billie">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to={'/'} className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="/src/assets/img/logoBarra.svg" className="h-8" alt="Billie Eilish Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Eilish Vibes</span>
                     </Link>
-                    <button data-collapse-toggle="navbar-dropdown" type="button"
+                    <button
+                        onClick={() => setMenuAbierto(!menuAbierto)}
                         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-dropdown" aria-expanded="false">
+                        aria-controls="navbar-dropdown" aria-expanded={menuAbierto ? "true" : "false"}>
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                             viewBox="0 0 17 14">
@@ -21,7 +25,7 @@ export const NavbarComponent = () => {
                                 d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                    <div className={`${menuAbierto ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-dropdown">
                         <ul
                             className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
                             <li>
@@ -29,10 +33,15 @@ export const NavbarComponent = () => {
                                     className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                                     aria-current="page">Home</Link>
                             </li>
-                            <li>
-                                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-                                    className="flex items-center justify-between w-full py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                            <li className="relative">
+                                <button
+                                    onClick={() => setdesplegableTienda(!desplegableTienda)}
+                                    className="flex items-center justify-between w-full py-2 px-3 text-white rounded
+                                     hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto
+                                     dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700
+                                     dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                                     Store
+
                                     <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                         fill="none" viewBox="0 0 10 6">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -41,7 +50,7 @@ export const NavbarComponent = () => {
                                 </button>
                                 {/* <!-- Dropdown menu --> */}
                                 <div id="dropdownNavbar"
-                                    className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 component-background-billie  dark:divide-white">
+                                    className={`${desplegableTienda ? 'block' : 'hidden'} absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 component-background-billie dark:divide-white`}>
                                     <div className="py-1">
                                         <Link to={'/store'}
                                             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">All
@@ -131,8 +140,6 @@ export const NavbarComponent = () => {
                     </div>
                 </div>
             </nav>
-
-
         </>
-    )
-}
+    );
+};
